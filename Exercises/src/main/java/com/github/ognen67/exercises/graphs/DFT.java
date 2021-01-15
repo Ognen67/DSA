a2383b9f1a29f4ca72a6d4f1028663807c096485
Ognen Kirov Todorovski <todorovskiognen@gmail.com>
2021-01-15 14:39:36 +0100

    DFT Graph

--- Exercises/src/main/java/com/github/ognen67/exercises/graphs/DFT.java
+++ Exercises/src/main/java/com/github/ognen67/exercises/graphs/DFT.java
@@ -1,5 +1,30 @@
 package com.github.ognen67.exercises.graphs;
 
-public class DFT {
+import java.util.ArrayList;
+
+class DFT
+{
+    private ArrayList<ArrayList<Integer>> adj;
+    private boolean[] visited;
+    private ArrayList<Integer> result = new ArrayList<Integer>();
+
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        this.adj = adj;
+        this.visited = new boolean[V];
+        DFSVisit(0);// odavde brisemo visited
+        return result;
+    }
+
+    public void DFSVisit(int v) {
+
+        visited[v] = true; // odakle nam visited ovde? moramo ga proslediti
+        result.add(v);
+
+        for (int u: adj.get(v))
+            if (!visited[u]) DFSVisit(u); // i ako to u nije vec visited
+        // znaci ako postoji dete u cvora v koje nije poseceno, poseti to u odmah
+
+    }
 
 }
